bb7bd4b16e2afb3f199a73aee1f8429f71f122d0
Erkan Doğan <erkan7830667@gmail.com>
2022-05-15 18:00:12 +0300

    embed video2

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,11 @@ module.exports = {
                   embedURL: videoId =>
                     `https://www.youtube-nocookie.com/embed/${videoId}`,
                 },
+              ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
+              containerClass: "embedVideo-container", //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
+              iframeId: false, //Optional: if true, iframe's id will be set to what is provided after 'video:' (YouTube IFrame player API requires iframe id)
+            },
+          },
           {
             resolve: `gatsby-remark-images`,
             options: {
