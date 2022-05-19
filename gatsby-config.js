1ea1adda5b03b33b43c65c9cc913cae38721a538
Erkan Doğan <43936027+erkdgn@users.noreply.github.com>
2022-05-19 12:14:05 +0300

    update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Erkan Doğan`,
     author: {
-      name: `Erkan Doğan`,
+      name: `Erkan`,
       summary: ``,
     },
     description: `Kişisel Blog | Düşünceler, hikayeler, fikirler... `,
